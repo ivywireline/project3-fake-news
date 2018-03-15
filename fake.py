5c6321a01250c8b11e3eb7f2e46cec750d24de89
Kyle Zhou <kylezhou2002@hotmail.com>
2018-03-15 19:29:10 -0400

    Added word counting code

--- fake.py
+++ fake.py
@@ -1,4 +1,7 @@
 import random
+import json
+
+from collections import defaultdict
 
 real_filename = 'clean_real.txt'
 fake_filename = 'clean_fake.txt'
@@ -19,6 +22,14 @@ def load_headlines(headlines_filename):
 
     return training, validation, test
 
+def count_words(headlines):
+    word_counts = defaultdict(lambda: 0)
+    for line in headlines:
+        for word in line.split(' '):
+            word_counts[word] += 1
+
+    return word_counts
+
 if __name__ == '__main__':
     random.seed(0)
     real_training, real_validation, real_test = load_headlines(real_filename)
@@ -27,3 +38,9 @@ if __name__ == '__main__':
     print 'Num training', 'Num validation', 'Num test'
     print len(real_training), len(real_validation), len(real_test)
     print len(fake_training), len(fake_validation), len(fake_test)
+
+    real_counts = count_words(real_training)
+    fake_counts = count_words(fake_training)
+
+    print real_counts
+    print fake_counts
