d317d798615fef57d985184e2e4588944050b81f
Kyle Zhou <kylezhou2002@hotmail.com>
2018-03-16 16:13:30 -0400

    Classifier kinda working

--- fake.py
+++ fake.py
@@ -4,6 +4,8 @@ import json
 
 from collections import defaultdict
 
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+
 real_filename = 'clean_real.txt'
 fake_filename = 'clean_fake.txt'
 
@@ -40,11 +42,11 @@ def count_word_occurrance(headlines):
     '''
     word_counts = defaultdict(lambda: 0)
     for line in headlines:
-        already_found = []
+        already_found = set()
         for word in line.split(' '):
             if word not in already_found:
                 word_counts[word] += 1
-                already_found.append(word)
+                already_found.add(word)
 
     return word_counts
 
@@ -55,23 +57,37 @@ def train_model(real_headlines, fake_headlines, m, p):
     probabilities_real = {}
     probabilities_fake = {}
     for word in word_list:
+        # if word in ENGLISH_STOP_WORDS: continue
         if word in real_counts:
             probabilities_real[word] = (real_counts[word] + m * p) / float(len(real_headlines) + m)
         if word in fake_counts:
             probabilities_fake[word] = (fake_counts[word] + m * p) / float(len(fake_headlines) + m)
 
-    return probabilities_real, probabilities_fake, m, p, len(real_headlines), len(fake_headlines)
+    return probabilities_real, probabilities_fake, m, p, len(real_headlines), len(fake_headlines), word_list
 
 def predict_model(model, headline):
-    probabilities_real, probabilities_fake, m, p, real_count, fake_count = model
-    probability_real = 0.0
-    probability_fake = 0.0
-    for word in headline.split(' '):
-        probability_real += math.log((probabilities_real.get(word, 0) + m * p) / float(real_count + m))
-        probability_fake += math.log((probabilities_fake.get(word, 0) + m * p) / float(fake_count + m))
-    print probability_real
-    print probability_fake
-    return math.exp(probability_real) > math.exp(probability_fake)
+    probabilities_real, probabilities_fake, m, p, real_count, fake_count, word_list = model
+    logprob_real = 0.0
+    logprob_fake = 0.0
+    real_class_prob = float(real_count) / (real_count + fake_count)
+    fake_class_prob = float(fake_count) / (real_count + fake_count)
+    headline_split = headline.split(' ')
+    for word in word_list:
+        if word in headline_split:
+            if word in probabilities_real:
+                logprob_real += math.log(probabilities_real[word])
+            if word in probabilities_fake:
+                logprob_fake += math.log(probabilities_fake[word])
+        else:
+            if word in probabilities_real:
+                logprob_real += math.log(1 - probabilities_real[word])
+            if word in probabilities_fake:
+                logprob_fake += math.log(1 - probabilities_fake[word])
+        # if word in ENGLISH_STOP_WORDS: continue
+    real_prob = math.exp(logprob_real) * real_class_prob
+    fake_prob = math.exp(logprob_fake) * fake_class_prob
+    print real_prob, fake_prob
+    return real_prob > fake_prob
 
 
 if __name__ == '__main__':
@@ -89,11 +105,11 @@ if __name__ == '__main__':
     #print get_wordlist(real_training, fake_training)
 
     # These parameters need lots of tweaking
-    m = 100
-    p = 10
+    m = 1.0
+    p = 1.0
 
     model = train_model(real_training, fake_training, m, p)
-    print predict_model(model, real_validation[0])
+    print predict_model(model, fake_validation[1])
 
     # high_fake = [a for a in fake_counts if a in real_counts and fake_counts[a] > 3 and fake_counts[a] > real_counts[a]]
 
