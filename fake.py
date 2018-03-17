294761dbfa147237a08a9d4213f4bc9f4fc96cfd
Steven Tan <stevenhaohuitan@gmail.com>
2018-03-17 16:07:04 -0400

    part 4

--- fake.py
+++ fake.py
@@ -1,6 +1,13 @@
 import math
 import random
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
 
 from collections import defaultdict
 
@@ -8,6 +15,10 @@ from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 real_filename = 'clean_real.txt'
 fake_filename = 'clean_fake.txt'
+
+# Key is a unique word string. Value is the index of the word in unique_words_set
+unique_words_dict = {}
+
 
 def get_wordlist(*args):
     headlines = [hl for hls in args for hl in hls]
@@ -18,6 +29,7 @@ def get_wordlist(*args):
             word_list.add(w)
 
     return sorted(list(word_list))
+
 
 def load_headlines(headlines_filename):
     with open(headlines_filename, 'r') as infile:
@@ -134,6 +146,104 @@ def get_performance(model, real_training, fake_training, real_test, fake_test, r
 
     return performance_training, performance_test, performance_validation_real, performance_validation_fake, performance_validation_total
 
+############################# Logistic Regression #############################
+def process_headlines(real_training, fake_training):
+    # Get the unique words list. Its index is important for feature vector representation.
+    unique_words_set = get_wordlist(real_training, fake_training)
+    unique_words_number = len(unique_words_set)
+    idx = 0
+
+    for word in unique_words_set:
+        unique_words_dict[word] = idx
+        idx += 1
+
+
+def get_train(real_training, fake_training):
+    # 1 means real. 0 means fake.
+    unique_words_set = get_wordlist(real_training, fake_training)
+    unique_words_number = len(unique_words_set)
+
+    # Number of features is the number of the unique words in the training set
+    batch_xs = np.zeros((0, unique_words_number))
+    # There are only 2 classes - real or fake
+    batch_y_s = []
+
+    for headline in real_training:
+        # Vector simulating the headline
+        vector_headline = np.zeros(unique_words_number)
+        headline_words = headline.split(" ")
+        for word in headline_words:
+            index = unique_words_dict[word]
+            vector_headline[index] = 1
+        batch_xs = np.vstack((batch_xs, vector_headline))
+        batch_y_s.append(1)
+
+    for headline in fake_training:
+        # Vector simulating the headline
+        vector_headline = np.zeros(unique_words_number)
+        headline_words = headline.split(" ")
+        for word in headline_words:
+            index = unique_words_dict[word]
+            vector_headline[index] = 1
+        batch_xs = np.vstack((batch_xs, vector_headline))
+        batch_y_s.append(0)
+
+    return batch_xs, batch_y_s
+
+# Model
+class LogisticRegression(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+def part4():
+    # Hyper Parameters
+    input_size = 784
+    num_classes = 10
+    num_epochs = 5
+    batch_size = 100
+    learning_rate = 0.001
+
+    model = LogisticRegression(input_size, num_classes)
+
+    # Loss and Optimizer
+    # Softmax is internally computed.
+    # Set parameters to be updated.
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    # Training the Model
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = Variable(images.view(-1, 28*28))
+            labels = Variable(labels)
+
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 100 == 0:
+                print ('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f'
+                       % (epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+
+    # Test the Model
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = Variable(images.view(-1, 28*28))
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+    print('Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
 
 if __name__ == '__main__':
     random.seed(0)
@@ -150,23 +260,18 @@ if __name__ == '__main__':
     #print get_wordlist(real_training, fake_training)
 
     # These parameters need lots of tweaking
-    m = 1.0
-    p = 1.0
+    # m = 1.0
+    # p = 1.0
+    #
+    # model = train_model(real_training, fake_training, m, p)
+    #
+    # performance_training, performance_test, performance_validation_real, performance_validation_fake, performance_validation_total = get_performance(model, real_training, fake_training, real_test, fake_test, real_validation, fake_validation)
 
-    model = train_model(real_training, fake_training, m, p)
+    process_headlines(real_training, fake_training)
+    batch_xs, batch_y_s = get_train(real_training, fake_training)
 
-    performance_training, performance_test, performance_validation_real, performance_validation_fake, performance_validation_total = get_performance(model, real_training, fake_training, real_test, fake_test, real_validation, fake_validation)
-
-    print "performance for training set is", performance_training
-
-    print "performance for test set is", performance_test
-
-    print "performance for validation set for real headlines is", performance_validation_real
-
-    print "performance for validation set for fake headlines is", performance_validation_fake
-
-    print "performance for the entire validation set is", performance_validation_total
-
+    print "batch_xs", batch_xs
+    print "batch_y_s", batch_y_s
     # high_fake = [a for a in fake_counts if a in real_counts and fake_counts[a] > 3 and fake_counts[a] > real_counts[a]]
 
     # print high_fake
